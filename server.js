d749f186fc82249cbb1ecbdd5ed6f32214d70a0a
eduardo329 <eduardo_rj_18@hotmail.com>
2022-12-06 17:53:14 -0300

    Compilando eJS

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(requisition, response){
-    response.render("index");
-})
+app.get("/", function (requisition, response) {
+  const items = [
+    { 
+        title: "D", 
+        message: "Desenvolver aplicações/serviços de forma fácil"
+        
+    },
+    {
+        title: "E",
+        message: "EJS usa JavaScript para renderizar HTML"
+    },
+    {
+        title: "M",
+        message: "Multi fácil de usar"
+    },
+    {
+        title: "A",
+        message: "Amorzinho de todos"
+    },
+    {
+        title: "i",
+        message: "Install ejs"
+    },
+    {
+        title: "S",
+        message: "Sintaxe simples"
+    }
+  ];
+  response.render("pages/index",
+    {
+        qualitys: items,
+
+    });
+});
 
 app.get("/sobre", function (requisition, response) {
-    response.render("about");
-})
+  response.render("pages/about");
+});
 
 app.listen(8080);
-console.log('Server initialized')
+console.log("Server initialized");
